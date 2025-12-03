79bf320265c6c74345d84747cbe90c37e54161f4
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2025-12-03 22:05:28 +0100

    day2 part2

--- src/main/java/se/johannesdahlgren/aoc25/Day2.java
+++ src/main/java/se/johannesdahlgren/aoc25/Day2.java
@@ -3,6 +3,7 @@ package se.johannesdahlgren.aoc25;
 import se.johannesdahlgren.util.FileUtil;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Day2 {
 
@@ -12,7 +13,7 @@ public class Day2 {
     productIdRanges = FileUtil.toStringFromCsv(FileUtil.getPath("2025", "day2." + fileName));
   }
 
-  public long sumInvalidIds() {
+  public long sumInvalidIds(boolean atLeastTwice) {
     long sum = 0;
     for (String productIdRange : productIdRanges) {
       long subSum = 0;
@@ -21,13 +22,30 @@ public class Day2 {
       long upper = Long.parseLong(split[1]);
       for (long i = lower; i <= upper; i++) {
         String id = String.valueOf(i);
-        if (hasRepeatingTwiceSequence(id)) {
+        if (hasRepeatingTwiceSequence(id) || (atLeastTwice && hasRepeatingSequence(id))) {
           subSum += i;
         }
       }
       sum += subSum;
     }
     return sum;
+  }
+
+  private boolean hasRepeatingSequence(String id) {
+    for (int i = 1; i <= id.length() / 2; i++) {
+      int finalI = i;
+      int start = 0;
+      int end = (id.length() + i - 1) / i;
+      List<String> list = IntStream
+          .range(start, end)
+          .mapToObj(j -> id.substring(j * finalI, Math.min((j + 1) * finalI, id.length())))
+          .toList();
+
+      if (list.size() >= 2 & list.stream().allMatch(s -> list.getFirst().equals(s))) {
+        return true;
+      }
+    }
+    return false;
   }
 
   private boolean hasRepeatingTwiceSequence(String id) {
--- src/test/java/se/johannesdahlgren/aoc25/Day2Test.java
+++ src/test/java/se/johannesdahlgren/aoc25/Day2Test.java
@@ -9,12 +9,24 @@ class Day2Test {
   @Test
   void part1Example() {
     Day2 day2 = new Day2("example");
-    assertThat(day2.sumInvalidIds()).isEqualTo(1227775554L);
+    assertThat(day2.sumInvalidIds(false)).isEqualTo(1227775554L);
   }
 
   @Test
   void part1() {
     Day2 day2 = new Day2("");
-    assertThat(day2.sumInvalidIds()).isEqualTo(40398804950L);
+    assertThat(day2.sumInvalidIds(false)).isEqualTo(40398804950L);
+  }
+
+  @Test
+  void part2Example() {
+    Day2 day2 = new Day2("example");
+    assertThat(day2.sumInvalidIds(true)).isEqualTo(4174379265L);
+  }
+
+  @Test
+  void part2() {
+    Day2 day2 = new Day2("");
+    assertThat(day2.sumInvalidIds(true)).isEqualTo(65794984339L);
   }
 }
